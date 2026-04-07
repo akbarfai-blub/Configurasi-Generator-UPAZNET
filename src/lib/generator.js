50d4211326e58eb02ad13230cb6fd5943708c49b
AkbarF <fy190322@gmail.com>
2026-04-07 07:07:38 +0700

    adjusting generator c600 c320

--- src/lib/generator.js
+++ src/lib/generator.js
@@ -145,7 +145,8 @@ export const generateUGR = (data) => {
   const cleanId = idPelanggan.toString().slice(0, 10);
   const vlan = "1000"; // VLAN khusus UGR
 
-  return `interface gpon_olt-${interfaceOlt}
+  return `config terminal
+interface gpon_olt-${interfaceOlt}
 onu ${onuId} type ALL sn ${sn}
 exit
 interface gpon_onu-${interfaceOlt}:${onuId}
@@ -174,25 +175,27 @@ export const generateUNB = (data) => {
 
   // Mapping Detail untuk setiap jenis UNB
   const unbConfigs = {
-    "100": { vlan: "100", profile: "pppoe", type: "standard", tcontProfile: "kusuma", onuType: "ALL" },
-    "1600": { vlan: "1600", profile: "vlan1600", type: "standard", tcontProfile: "kusuma", onuType: "ALL" },
-    "1501": { vlan: "1501", profile: "bolo", type: "standard", tcontProfile: "kusuma", onuType: "ALL" },
-    "602": { vlan: "602", profile: "vlan602", type: "standard", tcontProfile: "metro10", onuType: "ALL-ONT" },
-    "903": { vlan: "903", profile: "vlan903", type: "lexxa", tcontProfile: "default", onuType: "ALL-ONT" },
-    "511": { vlan: "511", profile: "vlan511", type: "standard", tcontProfile: "kusuma", onuType: "ALL" },
+    "100": { vlan: "100", profile: "pppoe", type: "standard", tcontProfile: "kusuma", onuType: "ALL", useC300Syntax: true },
+    "1600": { vlan: "1600", profile: "vlan1600", type: "standard", tcontProfile: "kusuma", onuType: "ALL", useC300Syntax: true },
+    "1501": { vlan: "1501", profile: "bolo", type: "standard", tcontProfile: "kusuma", onuType: "ALL", useC300Syntax: true },
+    "602": { vlan: "602", profile: "vlan602", type: "standard", tcontProfile: "metro10", onuType: "ALL-ONT", useC300Syntax: true },
+    "903": { vlan: "903", profile: "vlan903", type: "lexxa", tcontProfile: "default", onuType: "ALL-ONT", useC300Syntax: true },
+    "511": { vlan: "511", profile: "vlan511", type: "standard", tcontProfile: "kusuma", onuType: "ALL", useC300Syntax: true },
     "bridge_unb": { vlan1: "105", vlan2: "102", profile: "pppoe_vlan102", type: "bridge" },
     "bridge_bolo": { vlan1: "1500", vlan2: "1501", profile: "bolo", type: "bridge_bolo" }
   };
 
   const conf = unbConfigs[selectedVlanType];
+  const oltPrefix = conf && conf.useC300Syntax ? "gpon-olt_" : "gpon_olt-";
+  const onuPrefix = conf && conf.useC300Syntax ? "gpon-onu_" : "gpon_onu-";
 
   // LOGIK UNB BRIDGE STANDAR
   if (conf.type === "bridge") {
     return `conf t
-interface gpon_olt-${interfaceOlt}
+interface ${oltPrefix}${interfaceOlt}
 onu ${onuId} type ALL sn ${sn}
 exit
-interface gpon_onu-${interfaceOlt}:${onuId}
+interface ${onuPrefix}${interfaceOlt}:${onuId}
 name ${cleanId}
 description ${cleanId}_bridge
 sn-bind enable sn
@@ -202,7 +205,7 @@ gemport 2 tcont 1
 service-port 1 vport 1 user-vlan ${conf.vlan1} vlan ${conf.vlan1}
 service-port 2 vport 2 user-vlan ${conf.vlan2} vlan ${conf.vlan2}
 exit
-pon-onu-mng gpon_onu-${interfaceOlt}:${onuId}
+pon-onu-mng ${onuPrefix}${interfaceOlt}:${onuId}
 service ${conf.vlan1} gemport 1 vlan ${conf.vlan1}
 service pppoe gemport 2 vlan ${conf.vlan2}
 vlan port eth_0/1 mode tag vlan ${conf.vlan1}
@@ -219,10 +222,10 @@ write`.trim();
   // LOGIK UNB BRIDGE BOLO
   if (conf.type === "bridge_bolo") {
     return `conf t
-interface gpon_olt-${interfaceOlt}
+interface ${oltPrefix}${interfaceOlt}
 onu ${onuId} type ALL sn ${sn}
 exit
-interface gpon_onu-${interfaceOlt}:${onuId}
+interface ${onuPrefix}${interfaceOlt}:${onuId}
 name ${cleanId}
 description ${cleanId}
 sn-bind enable sn
@@ -232,7 +235,7 @@ gemport 2 tcont 1
 service-port 1 vport 1 user-vlan ${conf.vlan1} vlan ${conf.vlan1}
 service-port 2 vport 2 user-vlan ${conf.vlan2} vlan ${conf.vlan2}
 exit
-pon-onu-mng gpon_onu-${interfaceOlt}:${onuId}
+pon-onu-mng ${onuPrefix}${interfaceOlt}:${onuId}
 service ${conf.vlan1} gemport 1 vlan ${conf.vlan1}
 service pppoe gemport 2 vlan ${conf.vlan2}
 vlan port eth_0/1 mode hybrid def-vlan ${conf.vlan1}
@@ -245,16 +248,16 @@ exit
 write`.trim();
   }
 
-  // LOGIK UNB STANDARD (V100, V1600, V1501, 602, 903)
+  // LOGIK UNB STANDARD (V100, V1600, V1501, 602, 903, 511)
   const onuType = conf.onuType || "ALL";
   const tcontProfile = conf.tcontProfile || "kusuma";
   const isLexxa = conf.type === "lexxa";
 
   return `conf t
-interface gpon_olt-${interfaceOlt}
+interface ${oltPrefix}${interfaceOlt}
 onu ${onuId} type ${onuType} sn ${sn}
 exit
-interface gpon_onu-${interfaceOlt}:${onuId}
+interface ${onuPrefix}${interfaceOlt}:${onuId}
 name ${cleanId}
 description ${cleanId}
 sn-bind enable sn
@@ -263,7 +266,7 @@ gemport 1 name PPPOE tcont 1
 ${isLexxa ? "encrypt 1 enable downstream\n" : ""}switchport mode hybrid vport 1
 service-port 1 vport 1 user-vlan ${conf.vlan} vlan ${conf.vlan}
 exit
-pon-onu-mng gpon_onu-${interfaceOlt}:${onuId}
+pon-onu-mng ${onuPrefix}${interfaceOlt}:${onuId}
 service ServiceName gemport 1 cos 0 vlan ${conf.vlan}
 wan-ip 1 mode pppoe username ${pppoeUser} password ${pppoePass} vlan-profile ${conf.profile} host 1
 wan-ip 1 ping-response enable traceroute-response enable
@@ -278,7 +281,7 @@ write`.trim();
  */
 export const generateMikrotikSecret = (data) => {
   const { idPelanggan, namaPelanggan, pppoeUser, pppoePass, paketLayanan } = data;
-  
+
   // Format Comment: "ID-NAMA"
   const commentText = namaPelanggan ? `${idPelanggan}-${namaPelanggan.toUpperCase()}` : idPelanggan;
 
