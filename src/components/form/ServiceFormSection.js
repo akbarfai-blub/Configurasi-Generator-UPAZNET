cdf7cd56927e7e9a2fd4c806d3818f9be6c97748
AkbarF <fy190322@gmail.com>
2026-05-03 07:22:06 +0700

    fixing serviceformsection

--- src/components/form/ServiceFormSection.js
+++ src/components/form/ServiceFormSection.js
@@ -15,14 +15,14 @@ export default function ServiceFormSection({ formData, onChange }) {
             className="w-full p-2.5 border border-slate-300 bg-slate-50 rounded-lg focus:ring-2 focus:ring-upaz-green focus:border-upaz-green outline-none transition font-semibold text-sm text-slate-800"
           >
             <optgroup label="Paket Kusuma">
-              <option value="KUSUMA 1">KUSUMA 0</option>
+              <option value="KUSUMA 0">KUSUMA 0</option>
               <option value="KUSUMA 1">KUSUMA 1</option>
               <option value="KUSUMA 2">KUSUMA 2</option>
               <option value="KUSUMA 3">KUSUMA 3</option>
               <option value="KUSUMA 4">KUSUMA 4</option>
             </optgroup>
             <optgroup label="Paket Puspa">
-              <option value="PUSPA 1">PUSPA 0</option>
+              <option value="PUSPA 0">PUSPA 0</option>
               <option value="PUSPA 1">PUSPA 1</option>
               <option value="PUSPA 2">PUSPA 2</option>
               <option value="PUSPA 3">PUSPA 3</option>
